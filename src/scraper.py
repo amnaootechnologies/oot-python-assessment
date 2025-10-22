a6561a486b5ec8d7d2c4db1c0deb0fad8c2f84fb
amnaootechnologies <amna.ootechnologies@gmail.com>
2025-10-22 16:54:02 +0500

    Delete src directory

--- src/scraper.py
+++
@@ -1,9 +0,0 @@
-"""
-OOTechnologies Web Scraping Assessment
---------------------------------------
-Implement your scraping logic here.
-
-- Target: https://books.toscrape.com
-- Output: data/books.json
-- Fields: title, price, availability, rating, product_url
-"""
